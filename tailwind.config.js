250c16833b16aa1ec4dfeaf4c6a390f006d496b8
lums <jeremy@lums.io>
2023-09-25 12:32:22 +0200

    Add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
 module.exports = {
-  mode: "jit",
-  darkMode: "media", // or 'media' or 'class'
+  mode: 'jit',
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cdu-mint': '#52b7c1',
+        'cdu-darkblue': '#2d3c4b',
+        'cdu-white': '#fff',
+        'cdu-gold': '#ffa600',
+        'cdu-red': '#bf111b',
+        'cdu-black': '#1b191d',
+        'afd-blue': '#0099ff',
+        'afd-blue-dark': '#0033cc',
+        'afd-blue-dark': '#cc0000',
+        'afd-gradient-second': '#0072b3',
+        'lums-accent': '#A1E8AF',
+      },
+    },
   },
   variants: {},
   plugins: [],
-  content: ["./_site/**/*.{md,njk,sass}"],
+  content: ['./_site/**/*.{md,njk,sass}'],
 };
