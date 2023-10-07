791881f9237b77906bca6a0a10b23980bd4f77af
lumsdnb <jeremy@lums.io>
2023-10-07 09:26:42 +0200

    Add Telegram discussions

---
+++ src/lib/CommentBox.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { Discussions } from 'sveltegram';
+</script>
+
+<section>
+  <h2 class="md:text-9xl text-5xl font-partei scale-110 text-center p-10 py-10">
+    Noch Fragen?
+  </h2>
+  <Discussions link="https://t.me/werteverbinden/3" />
+</section>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,15 @@
   import '../app.css';
   import Footer from '$lib/assets/Footer.svelte';
   import RainbowLine from '$lib/RainbowLine.svelte';
+  import CommentBox from '$lib/CommentBox.svelte';
 </script>
 
 <svelte:head><title>Werte verbinden</title></svelte:head>
 <main class="m-0 p-0 text-lg w-full flex flex-col justify-center">
   <RainbowLine />
   <slot />
+
+  <CommentBox />
 
   <Footer />
 </main>
